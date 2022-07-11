21bd0fb0c33b0a67ab9454348662cf0294efe46a
Jashanpreet <jashanprt@gmail.com>
2022-07-11 22:19:05 +0530

    DataTypes Added

---
+++ datatype.js
@@ -0,0 +1,27 @@
+var str="This is string";
+var str1='This is string 2';
+var num1=120;
+var num2=10.1;
+
+// Objects
+var sample={
+    name: "jashan",
+    age: 10
+};
+
+var a=true;
+var b=false;
+
+var un=undefined;
+var abc;
+var nl=null;
+
+console.log(str);
+console.log(str1);
+console.log(num1);
+console.log(num2);
+console.log(sample);
+
+console.log(un);
+console.log(abc);
+console.log(nl);
